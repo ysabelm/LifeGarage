187186142e93404e476c754610aac07c3a0e59e6
Isabelle Monfort <ysabelmonfort@gmail.com>
2017-01-30 16:51:37 +0100

    App LifeGarage

---
+++ Bike.java
@@ -0,0 +1,15 @@
+package com.LifeGarage;
+
+
+public class Bike extends Vehicle{
+
+    public Bike(String brand, Color color, float price){
+        super(brand, color, price);
+    }
+    @Override
+    public float getPriceWithTax(){
+        int tax = 1;
+        return price + tax;
+    }
+
+}
