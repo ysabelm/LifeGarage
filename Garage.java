b5baa031cfcace181b112d4c640ad3b4be8f344d
Isabelle Monfort <ysabelmonfort@gmail.com>
2017-01-30 16:48:18 +0100

    App LifeGarage

---
+++ Garage.java
@@ -0,0 +1,45 @@
+package com.LifeGarage;
+
+import java.util.List;
+
+
+public class Garage {
+    private String name;
+    private List<Car> cars;
+    private List<Bike> bikes;
+
+    public Garage(String name, List<Car> cars, List<Bike> bikes){
+        this.name = name;
+        this.cars = cars;
+        this.bikes = bikes;
+    }
+    public String getName(){
+        return name;
+    }
+    public List<Car> getCars(){
+        return cars;
+    }
+    public List<Bike> getBikes(){
+        return bikes;
+    }
+    public void setCars(List<Car> cars){
+        return cars;
+    }
+    public void setBikes(List<Bike> bikes){
+        return bikes;
+    }
+    public void addCar(Car car){
+        cars.add(car);
+    }
+    public void addBike(Bike bike){
+        bikes.add(bike);
+    }
+    @Override
+    public String toString(){
+        return "Garage{" +
+                "name='" + name + '\'' +
+                ", cars='" + cars + '\'' +
+                ", bikes='" + bikes +
+                '}'
+    }
+}
